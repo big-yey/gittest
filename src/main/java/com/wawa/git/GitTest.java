ee2c5d0b021c6a7b049e70fb57a61fd0f0fd94e3
wawa <wawa@163.com>
2022-11-29 11:49:50 +0800

    hot-fix two

--- src/main/java/com/wawa/git/GitTest.java
+++ src/main/java/com/wawa/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git hot-fix");
+        System.out.println("hello git hot-fix22222222222");
+
     }
 }
